4425aec307970652932d7d4ba6cefcd09fa6df56
phate <nico.reissmann@gmail.com>
2010-03-12 12:16:39 +0100

    Added arguments to main function; yylex does not read from stdin anymore

--- main.c
+++ main.c
@@ -9,20 +9,66 @@
 #include <stdlib.h>
 
 extern node* parse() ;
+extern FILE *yyin ;
+FILE *ad = NULL ;
+FILE *hf = NULL ;
+FILE *sf = NULL ;
+
+static void cleanup()
+{
+	//TODO: more todo
+	fclose( ad ) ;
+	fclose( hf ) ;
+	fclose( sf ) ;
+}
+
+static FILE* open_file( const char *filename, const char *mode )
+{
+	FILE *f = fopen( filename, mode ) ;
+	if( f == NULL ){
+		fprintf( stderr, "Cannot open file: %s", filename ) ;
+		cleanup() ;
+		exit( EXIT_FAILURE ) ; 
+	}
+
+	return f ;
+}
+
+static void process_input( int argc, char* argv[] )
+{
+	if( argc != 4 ){
+		fprintf( stderr, "Invalid number of arguments:\n" ) ;
+		fprintf( stderr, "%s <arch description> <output header file> <output source file>\n", argv[0] ) ;
+		exit( EXIT_FAILURE ) ;
+	}
+
+	ad = open_file( argv[1], "r" ) ;
+	hf = open_file( argv[2], "w" ) ;
+	sf = open_file( argv[3], "w" ) ;	
+} 
 
 static void report_errors( node *n )
 {
 	uint32_t cnt = print_msgs( n ) ;
-	if( cnt != 0 ) exit( EXIT_FAILURE ) ;
-} 
+	if( cnt != 0 ){
+		cleanup() ;
+	 	exit( EXIT_FAILURE ) ;
+	}
+}
 
-int main()
+int main( int argc, char* argv[] )
 {
+	process_input( argc, argv ) ;
+	yyin = ad ;
+
 	node* ast ;
 	symtab stab = create_symtab() ;
 	
 	ast = parse() ;
-	if( ast == NULL ) exit( EXIT_FAILURE ) ;
+	if( ast == NULL ){
+		cleanup() ;
+		exit( EXIT_FAILURE ) ;
+	}
 	view_tree(ast) ;
 	
 	trim_tree( ast ) ;
@@ -35,6 +81,6 @@ int main()
 	typecheck( stab, ast ) ;
 	report_errors( ast ) ;
 
-
+	cleanup() ;
 	return 0 ;
 }
