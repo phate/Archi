4ee109d5dae1a1c81eb1f4e6e8b8f2729c6b4ae2
phate <nico.reissmann@gmail.com>
2010-07-29 00:53:58 +0200

    Implemented typechecking for ipattern without checking whether outputs of nodes have already been defined

--- tc/patternsect.c
+++ tc/patternsect.c
@@ -3,6 +3,37 @@
 #include "../ast/node.h"
 #include "../debug.h"
 
+static void archi_ipattern_typecheck( archi_symtab *st, archi_ast_node *n )
+{
+  DEBUG_ASSERT( st && n && n->node_type == NT_IPATTERN ) ;
+
+  archi_symtab_idlist *tf ;
+  archi_symtab_idlist *tl = archi_symtab_idlist_fill( NULL, st, NT_JVINSTRDEF ) ;
+
+  archi_ast_node *c ;
+  FOREACH_CHILD( n, c ){
+    DEBUG_ASSERT( c->node_type == NT_NODEDEF ) ;
+    tf = archi_variabledef_typecheck( st, c, c->attr.nt_nodedef.id, tl ) ; 
+
+    if( tf ){
+      archi_ast_node *l = archi_symtab_lookup( st, archi_symtab_idlist_id(tf) ) ;
+      DEBUG_ASSERT(l) ;
+
+      int32_t niex = l->attr.nt_jvinstrdef.ninputs ;
+      int32_t nig = c->attr.nt_nodedef.input->attr.nt_input.nchildren ;
+      int32_t noex = l->attr.nt_jvinstrdef.noutputs ;
+      int32_t nog = c->attr.nt_nodedef.output->attr.nt_output.nchildren ;
+      if( niex !=  nig )
+        archi_add_emsg( c, "expected %d inputs, got %d inputs", niex, nig ) ;
+
+      if( noex != nog  ) 
+        archi_add_emsg( c, "expected %d outputs, got %d outputs", noex, nog ) ;
+    }
+  } 
+
+  TALLOC_FREE( tl ) ;
+}
+
 static void archi_matchdef_typecheck( archi_symtab *st, archi_ast_node *n )
 {
   DEBUG_ASSERT( st && n && n->node_type == NT_MATCHDEF ) ;
@@ -26,8 +57,9 @@ static void archi_matchdef_typecheck( archi_symtab *st, archi_ast_node *n )
   }
   else EMSG_MISSING_ATTRIBUTE( n, "output" ) ;
 
-  if( n->attr.nt_matchdef.ipattern == NULL )
-    EMSG_MISSING_ATTRIBUTE( n, "ipattern" ) ;
+  if( n->attr.nt_matchdef.ipattern != NULL )
+    archi_ipattern_typecheck( st, n->attr.nt_matchdef.ipattern ) ;
+  else EMSG_MISSING_ATTRIBUTE( n, "ipattern" ) ;
 
   if( n->attr.nt_matchdef.opattern == NULL )
     EMSG_MISSING_ATTRIBUTE( n, "opattern" ) ;
